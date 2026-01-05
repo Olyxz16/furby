9de738d5ad026b5e6439b4c497f5ccb93b736933
Olyxz16 <cedric.sazos@tutanota.com>
2026-01-05 14:43:10 +0100

    feat: add auth controller

--- api/http/server.ts
+++ api/http/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
-import { router } from 'agenda/agenda.controller';
+import { agendaRouter } from 'agenda/agenda.controller';
+import { authRouter } from 'auth/auth.controller';
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/agendas", router);
+app.use("/agendas", agendaRouter);
+app.use("/auth", );
